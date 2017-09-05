5095b8653f4a3db9c90ea61b50b70475e45baf49
Stephen Garabedian <spg1502@rit.edu>
2017-09-05 19:06:57 -0400

    Removing bells & whistles - only the basics

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -4,10 +4,5 @@
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
-	std::string response;
-	response = std::cin.get();
-	std::cin.get();
-
-	std::cout << response + ", is what you said" << std::endl;
 	std::cin.get();
 }
